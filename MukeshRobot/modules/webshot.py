f15ddbb3ac0f1d84cdd820e1e0d705a4dd4d624a
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-10-14 22:46:02 +0530

    Update webshot.py

--- MukeshRobot/modules/webshot.py
+++ MukeshRobot/modules/webshot.py
@@ -7,7 +7,7 @@ from MukeshRobot.utils.errors import capture_err
 __help__ = """
 » /webss *:* sᴇɴᴅs ᴛʜᴇ sᴄʀᴇᴇɴsʜᴏᴛ ᴏғ ᴛʜᴇ ɢɪᴠᴇɴ ᴜʀʟ.
 
-☆............𝙱𝚈 » [𝚅𝙸𝙿 𝙱𝙾𝚈](https://t.me/the_vip_boy)............☆
+☆............𝙱𝚈 » [ʀσᴄкყ](https://t.me/ROCKY_ISS_BACK)............☆
 """
 __mod_name__ = "⚡Wᴇʙsʜᴏᴛ⚡"
 

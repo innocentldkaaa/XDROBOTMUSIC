67e48f953fab229375e304e4d4ff286589e0759c
ʀσƈƙყ <146221888+ROCKY-D3V@users.noreply.github.com>
2023-10-14 22:28:19 +0530

    Update config.py

--- MukeshRobot/config.py
+++ MukeshRobot/config.py
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = "14050586" # integer value, dont use ""
-    API_HASH = "42a60d9c657b106370c79bb0a8ac560c"
+    API_ID = "28630777" # integer value, dont use ""
+    API_HASH = "41962bd1035fcbd5c0243cec216a6e23"
     TOKEN = ""  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = "1808943146" # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "THE_VIP_BOY"
-    SUPPORT_CHAT = "TG_FRIENDSS"  # Your own group for support, do not add the @
+    OWNER_ID = "6545714937" # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "ROCKY_ISS_BACK"
+    SUPPORT_CHAT = "XD_N3TWORK"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001723893843
+        -1001885729197
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001723893843
+        -1001885729197
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
     MONGO_DB_URI= "mongodb+srv://hnyx:wywyw2@cluster0.9dxlslv.mongodb.net/?retryWrites=true&w=majority"
     # RECOMMENDED
@@ -39,15 +39,15 @@ class Config(object):
 
     # OPTIONAL
     ##List of id's -  (not usernames) for users which have sudo access to the bot.
-    DRAGONS = get_user_list("elevated_users.json", "1808943146")
+    DRAGONS = get_user_list("elevated_users.json", "6545714937")
     ##List of id's - (not usernames) for developers who will have the same perms as the owner
-    DEV_USERS = get_user_list("elevated_users.json", "1808943146")
+    DEV_USERS = get_user_list("elevated_users.json", "6545714937")
     ##List of id's (not usernames) for users which are allowed to gban, but can also be banned.
-    DEMONS = get_user_list("elevated_users.json", "1808943146")
+    DEMONS = get_user_list("elevated_users.json", "6545714937")
     # List of id's (not usernames) for users which WONT be banned/kicked by the bot.
-    TIGERS = get_user_list("elevated_users.json", "1808943146")
+    TIGERS = get_user_list("elevated_users.json", "6545714937")
     WOLVES = get_user_list("elevated_users.json", "whitelists")
-    DONATION_LINK = "https://t.me/the_vip_boy" # EG, paypal
+    DONATION_LINK = "https://t.me/ROCKY_ISS_BACK" # EG, paypal
     CERT_PATH = None
     PORT = 5000
     DEL_CMDS = True  # Delete commands that users dont have access to, like delete /ban if a non admin uses it.

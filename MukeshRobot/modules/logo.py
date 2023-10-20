1c932b15939a101ac827409db9b4fc4eb9b27745
innocentldkaaa <97851605+innocentldkaaa@users.noreply.github.com>
2023-10-20 13:40:20 +0530

    Update logo.py

--- MukeshRobot/modules/logo.py
+++ MukeshRobot/modules/logo.py
@@ -168,7 +168,7 @@ async def lego(event):
             os.remove(fname)
 
 
-__mod_name__ = "📍ʟᴏɢᴏ📍"
+__mod_name__ = "💞 ʟᴏɢᴏ 💞"
 
 __help__ = """
 I can create some beautiful and attractive logo for your profile pics.
